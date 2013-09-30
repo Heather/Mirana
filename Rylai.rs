9771d6e083e5df37c969a5c2c1e6af503226241f
Heather <heather@cynede.net>
2013-09-30 16:40:57 +0400

    matching do result for error handling o_O parsing json

--- Rylai.rs
+++ Rylai.rs
@@ -1,16 +1,28 @@
+extern mod extra;
+
 use std::os::change_dir;
 use std::str::from_utf8_owned;
 use std::run::process_output;
 
+/*  use std::rt::io::file::FileStream;
+    use std::rt::io::file::FileReader;
+    */
+
+use extra::json;
+use extra::json::*;
+use extra::serialize::{Decodable, Encodable};
+use extra::treemap::TreeMap;
+use std::io;
+
 static r_version: &'static str = "  Rylai v0.0.1";
-enum VCS {
-    git,
-    hg
-    }
-struct Repository {
-    loc: ~str,
-    t: VCS
-    }
+
+#[deriving(Encodable, Decodable)]
+struct WorkMap(TreeMap<Repository, ~str>);
+#[deriving(Encodable, Decodable, Eq, TotalEq, TotalOrd)]
+enum VCS { git, hg }
+#[deriving(Encodable, Decodable, Eq, TotalEq, TotalOrd)]
+struct Repository { loc: ~str, t: VCS }
+
 fn e(cmd: ~str, args : &[~str]) {
     let out = process_output(cmd, args);
     let msg = fmt!("> %s", from_utf8_owned(out.output.clone()));
@@ -22,10 +34,27 @@ fn gitSync(r: Repository) {
     change_dir( & Path( r.loc ) );
     e(~"git", [~"pull"]);
     }
+fn load_RepoList(p: &Path) -> Option<WorkMap> {
+    match do io::file_reader(p).map |rdr| {
+        json::from_reader(*rdr).expect("Repo list is broken")
+        }
+    { // Yes it's match + do
+        Err(_) => None,
+        Ok(json) => Decodable::decode(&mut json::Decoder(json))
+        }
+    }
+
 fn main() {
     println("_________________________________________________________________________");
     println(fmt!("    %s", r_version));
     println("_________________________________________________________________________");
+    
+    let mut t = WorkMap(TreeMap::new());
+    t.insert(Repository{ loc: ~"../NemerleWeb", t: git}, ~"NWeb");
+    
+    let encf = io::file_writer(& Path( "zz" ), [io::Create, io::Truncate]).unwrap();
+    t.encode(&mut json::Encoder(encf));
+    
     let mut total = 0;
     let myRepo = Repository { loc: ~"../NemerleWeb", t: git };
     match myRepo.t {
