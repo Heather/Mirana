f0a38980179732a3f0d3fd231022c5405328e2a9
Heather <heather@cynede.net>
2013-10-01 09:30:22 +0400

    Read config, iterate

--- Rylai.rs
+++ Rylai.rs
@@ -12,16 +12,13 @@ use std::run::process_output;
 use extra::json;
 use extra::json::*;
 use extra::serialize::{Decodable, Encodable};
-use extra::treemap::TreeMap;
 use std::io;
 
 static r_version: &'static str = "  Rylai v0.0.1";
 
-#[deriving(Encodable, Decodable)]
-struct WorkMap(TreeMap<Repository, ~str>);
-#[deriving(Encodable, Decodable, Eq, TotalEq, TotalOrd)]
+#[deriving(Encodable, Decodable, Clone)]
 enum VCS { git, hg }
-#[deriving(Encodable, Decodable, Eq, TotalEq, TotalOrd)]
+#[deriving(Encodable, Decodable, Clone)]
 struct Repository { loc: ~str, t: VCS }
 
 fn e(cmd: ~str, args : &[~str]) {
@@ -30,48 +27,47 @@ fn e(cmd: ~str, args : &[~str]) {
     let err = fmt!(" %s", from_utf8_owned(out.error.clone()));
     println(msg);
     println(err);
-    }
-fn gitSync(r: Repository) {
+}
+fn gitSync(r: &Repository) {
     change_dir( & Path( r.loc ) );
     e(~"git", [~"pull"]);
-    }
-fn load_RepoList(p: &Path) -> Option<WorkMap> {
+}
+fn load_RepoList(p: &Path) -> ~[Repository] {
     match do io::file_reader(p).map |rdr| {
         json::from_reader(*rdr).expect("Repo list is broken")
-        }
-    { // Yes it's match + do
-        Err(_) => None,
+    } { Err(_) => ~[],
         Ok(json) => Decodable::decode(&mut json::Decoder(json))
-        }
     }
+}
 
 fn main() {
     println("_________________________________________________________________________");
     println(fmt!("    %s", r_version));
     println("_________________________________________________________________________");
     
-    // Maybe later I will need to have ability to write conf from commandline
-    let mut t = WorkMap(TreeMap::new());
-    t.insert(Repository{ loc: ~"../NemerleWeb", t: git}, ~"NWeb");
-    let encf = io::file_writer(& Path( "repolist.conf" ), [io::Create, io::Truncate]).unwrap();
-    t.encode(&mut json::Encoder(encf));
+    let cfg = & Path ( "repolist.conf" );
+    let mut repoList = load_RepoList( cfg );
     
-    if (std::os::path_exists(& Path( "repolist.conf" ))) {
+    if (std::os::path_exists( cfg )) {        
         let mut total = 0;
-        let myRepo = Repository { loc: ~"../NemerleWeb", t: git };
-        match myRepo.t {
-            git => {
-                println(fmt!("    repo: %s", myRepo.loc));
-                gitSync(myRepo);
-                total += 1
+        for myRepo in repoList.iter() {
+           match myRepo.t {
+                git => {
+                    println(fmt!("    repo: %s", myRepo.loc));
+                    gitSync(myRepo);
+                    total += 1
                 }
-            _   => { println("not supported yet") }
+                _   => { println("not supported yet") }
             }
+        }
         println("_________________________________________________________________________");
         println(fmt!("  total    %?", total));
         println("_________________________________________________________________________");
-        }
-    else {
+    } else {
         println("No config file found, consider providing one");
-        }
+        println("For now one is created just for example");
+        repoList.push( Repository { loc: ~"../NemerleWeb", t: git } );
+        let encf = io::file_writer( cfg, [io::Create, io::Truncate]).unwrap();
+        repoList.encode(&mut json::Encoder(encf));
     }
+}
