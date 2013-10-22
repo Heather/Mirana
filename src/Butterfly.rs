5e48d9abbe14168a124b3bb57337540b08b5073d
Heather <heather@cynede.net>
2013-10-22 16:32:39 +0400

    println -> print

--- src/Butterfly.rs
+++ src/Butterfly.rs
@@ -8,11 +8,12 @@ pub fn butterfly<U>(f: &fn() -> U) -> U {
     let (port, chan) = stream();
     do task::spawn_sched(task::SingleThreaded) {
         while !port.peek() {
-            print("|");         sleep(100);
-            print("\x08/");     sleep(100);
-            print("\x08-");     sleep(100);
-            print("\x08\\");    sleep(100);
-            print("\x08");
+            print(" ");
+            let bug3911 = ["|","/","-","\\"];
+            bug3911.iter().map( |fly| {
+                print( format!("\x08{:s}", *fly) );
+                sleep(100);
+            });
         }
     }
     let ret = f();
