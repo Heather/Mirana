cc7baf12da76aed9da841c10945ac2301803d8bd
Heather <heather@cynede.net>
2013-10-22 16:38:26 +0400

    fastfix

--- src/Butterfly.rs
+++ src/Butterfly.rs
@@ -7,8 +7,8 @@ use std::rt::io::timer::sleep;
 pub fn butterfly<U>(f: &fn() -> U) -> U { 
     let (port, chan) = stream();
     do task::spawn_sched(task::SingleThreaded) {
+        print(" ");
         while !port.peek() {
-            print(" ");
             let bug3911 = ["|","/","-","\\"];
             for fly in bug3911.iter() {
                 print!("\x08{:s}", *fly);
