3648b18d21084f239f151aaafd5231bd5c62e91a
Heather <heather@cynede.net>
2013-10-23 16:32:47 +0400

    smarter Butterfly.rs

--- src/Butterfly.rs
+++ src/Butterfly.rs
@@ -4,14 +4,18 @@ use std::rt::io::timer::sleep;
 ///<Summary>
 ///Core how to Fly function
 ///</Summary>
-fn fly<U>(animation: &[&str], f: &fn() -> U) -> U {
+fn fly<U>(animation: &[&str], symbols: int, f: &fn() -> U) -> U {
     let howtofly = animation.map(|x|x.to_owned());
     let (port, chan) = stream();
     do task::spawn_sched(task::SingleThreaded) {
-        print(" ");
+        let mut prefix = ~"";
+        for _ in range (0, symbols) {
+            print(" ");
+            prefix = format!("{:s}\x08", prefix);
+        }
         while !port.peek() {
             for fly in howtofly.iter() {
-                print!("\x08{:s}", *fly);
+                print!("{:s}{:s}", prefix, *fly);
                 sleep(100);
             };
         }
@@ -26,5 +30,13 @@ fn fly<U>(animation: &[&str], f: &fn() -> U) -> U {
 ///</Summary>
 pub fn butterfly<U>(f: &fn() -> U) -> U {
     let animation = [&"|","/","-","\\"];
-    fly(animation, f)
+    fly(animation, 1, f)
+}
+
+///<Summary>
+///Stupid butterfly
+///</Summary>
+pub fn weird<U>(f: &fn() -> U) -> U {
+    let animation = [&"|","/","-","\\"];
+    fly(animation, 1, f)
 }
