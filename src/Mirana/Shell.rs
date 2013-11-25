2d19abfe8730a4069a57ebf21efe6c5741e33c2a
Heather <heather@cynede.net>
2013-11-25 11:36:38 +0400

    better shell execution method

--- src/Mirana/Shell.rs
+++ src/Mirana/Shell.rs
@@ -1,16 +1,5 @@
-use std::run::process_output;
+use std::run::{process_output, process_status};
 use std::str::from_utf8_owned;
-
-///<Summary>
-///Shell execute without error out
-///</Summary>
-#[inline]
-pub fn exec(cmd: &str, args : &[&str]) {
-    let oargs = args.map(|x|x.to_owned());
-    print(from_utf8_owned(
-            process_output(cmd, oargs)
-                .output.clone()));
-}
 
 ///<Summary>
 ///Shell execute without error, returns result
@@ -22,10 +11,27 @@ pub fn exe(cmd: &str, args : &[&str]) -> ~str {
 }
 
 ///<Summary>
-///Shell execute
+///Shell execute without error out
+///</Summary>
+#[inline]
+pub fn exec(cmd: &str, args : &[&str]) {
+    print( exe(cmd, args) );
+}
+
+///<Summary>
+///True Shell execute
 ///</Summary>
 #[inline]
 pub fn e(cmd: &str, args : &[&str]) {
+    let oargs = args.map(|x|x.to_owned());
+    process_status(cmd, oargs);
+}
+
+///<Summary>
+///Shell execute with process_output print
+///</Summary>
+#[inline]
+pub fn exy(cmd: &str, args : &[&str]) {
     let oargs = args.map(|x|x.to_owned());
     let out = process_output(cmd, oargs);
     let msg = from_utf8_owned(out.output.clone());
