74fbda9aa189d528507e29e2c5d326c5918a30d3
Heather <heather@cynede.net>
2013-12-02 15:25:53 +0400

    build script update

--- build.bat
+++ build.bat
@@ -1,15 +1,13 @@
 @echo off
-::setlocal
 rustc --version
+
+::clean
+rm -rf bin
+mkdir bin
+
+::compile
 cd src/Mirana
+rustc main.rs -O -o ../../bin/Mirana.exe
 
-rustc main.rs -O -o ../../Mirana.exe
-
-:: <--------------- rustpkg is buggy
-::mkdir .tmp
-::set TMPDIR=.tmp
-::rustpkg build Mirana
-
-::handle error messages
-::endlocal
+::wait / handle errors
 pause
